0c53cadfb86e4f1488a6c9acb92e54ddf3f3f918
beese <kristinbeese@gmail.com>
2016-02-14 21:40:40 -0500

    Created Xcode project and gitignore

---
+++ Nomad/Nomad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nomad
+//
+//  Created by Kristin Beese on 2/14/16.
+//  Copyright © 2016 Team 9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
