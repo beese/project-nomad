2efc2f680b158aa587b29e9d396b125e074d3772
beese <kristinbeese@gmail.com>
2016-02-21 11:40:08 -0500

    started model saving and loading

--- Nomad/Model.swift
+++ Nomad/Model.swift
@@ -8,23 +8,96 @@
 
 import Foundation
 
-public class Model {
+public class Model: NSObject, NSCoding {
     
     // MARK: Properties
-    var guID: NSUUID?
     
-    init? () {
-        // self.guID =
+    var guID: NSUUID
+    var rootFolder: NSString
+    
+    override convenience init() {
+        
+        // Generate GUID
+        self.init(guid: NSUUID())
+        
+    }
+    
+    init(guid: NSUUID) {
+        self.guID = guid
+        
+        // Finds the path to user's documents
+        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        
+        // Creates path to Nomad folder
+        self.rootFolder = documentsDirectory.stringByAppendingPathComponent("nomad")
+        
+        // Create Nomad Folder
+        // Will throw an error if the file had already been created
+        do {
+            try NSFileManager.defaultManager().createDirectoryAtPath(self.rootFolder as String, withIntermediateDirectories: false, attributes: nil)
+        } catch _ as NSError {
+            // file has already been created
+        }
+        
+    }
+    
+    
+    
+    
+    // MARK: NSCoding
+    
+    required convenience public init?(coder decoder: NSCoder) {
+        guard let guid = decoder.decodeObjectForKey("guid") as? NSUUID else {
+            return nil
+        }
+        
+        self.init(guid: guid)
+    }
+    
+    public func encodeWithCoder(coder: NSCoder) {
+        coder.encodeObject(self.guID, forKey: "guid")
+    }
+    
+    
+    
+    
+    // MARK: Methods
+    
+    func saveToDisk(path: String) {
+        do {
+            let folder = (path as NSString).stringByDeletingLastPathComponent
+            try NSFileManager.defaultManager().createDirectoryAtPath(folder, withIntermediateDirectories: true, attributes: nil)
+        } catch _ as NSError {
+            // file has already been created
+        }
+        
+        // Writes information to the disk
+        NSKeyedArchiver.archiveRootObject(self, toFile: path)
+        
+    }
+    
+    public static func loadFromDisk(path: String) -> Model? {
+        
+        // Loads information from the disk
+        guard let model = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as? [Model] else { return nil }
+        
+        // return the model loaded from the disk
+        return model[0]
+        
+    }
+    
+    func filePath() -> NSString {
+        
+        // Return the file path
+        return self.rootFolder
+        
     }
     
     func save() {
         
+        let path = filePath()
+        saveToDisk(path as String)
+        
     }
-    
-    fun load() {
-    
-    }
-    
-    
     
 }
--- Nomad/Trip.swift
+++ Nomad/Trip.swift
@@ -16,21 +16,110 @@ public class Trip: Model {
     var travelers: String
     var startDate: NSDate
     
+    // Read only computed property
+    var activeTrip: Bool {
+        get {
+            return endDate == nil
+        }
+    }
+    
     // optional variables
     public var endDate: NSDate?
-    public var startCoords: Float?
-    public var endCoords: Float?
+    public var startCoords: [Float]?
+    public var endCoords: [Float]?
     
-    init(title: String, travelers: String) {
-        self.title = title
-        self.travelers = travelers
+    
+    
+    // MARK: Initializers
+    
+    // When the user first creates a trip
+    init?(_title: String, _travelers: String, coords: [Float]?) {
+        
+        
+        title = _title
+        travelers = _travelers
+        
+        // Take the current time and save it in startDate
+        startDate = NSDate() // Format: Oct 12, 2015, 4:49 PM
+        
+        // May be nil if location services are off
+        startCoords = coords
+        
+        super.init(guid: NSUUID())
+        
+    }
+    
+    // Loading a trip off the disk
+    init?(_title: String, _travelers: String, _startCoords: [Float]?, _endCoords: [Float]?, _startDate: NSDate, _endDate: NSDate?, _guid: NSUUID) {
+        
+        title = _title
+        travelers = _travelers
+        
+        startCoords = _startCoords
+        endCoords = _endCoords
+        
+        startDate = _startDate
+        
+        // Should we create a bool to tell when a trip is currently active?
+        
+        endDate = _endDate
+        
+        super.init(guid: _guid)
+        
+    }
+    
+    // MARK: NSCoding
+
+    required convenience public init?(coder decoder: NSCoder) {
+        guard let title = decoder.decodeObjectForKey("title") as? String,
+            let travelers = decoder.decodeObjectForKey("travelers") as? String,
+            let startDate = decoder.decodeObjectForKey("start date") as? NSDate,
+            let endDate = decoder.decodeObjectForKey("end date") as? NSDate,
+            let startCoords = decoder.decodeObjectForKey("start coords") as? [Float],
+            let endCoords = decoder.decodeObjectForKey("end coords") as? [Float],
+            let guid = decoder.decodeObjectForKey("guid") as? NSUUID
+            else { return nil }
+        
+        self.init(
+            _title: title,
+            _travelers: travelers,
+            _startCoords: startCoords,
+            _endCoords: endCoords,
+            _startDate: startDate,
+            _endDate: endDate,
+            _guid: guid
+        )
+    }
+    
+    public override func encodeWithCoder(coder: NSCoder) {
+        
+        // encodes guid
+        super.encodeWithCoder(coder)
+        
+        coder.encodeObject(self.title, forKey: "title")
+        coder.encodeObject(self.travelers, forKey: "travelers")
+        coder.encodeObject(self.startDate, forKey: "start date")
+        coder.encodeObject(self.endDate, forKey: "end date")
+        coder.encodeObject(self.startCoords, forKey: "start coords")
+        coder.encodeObject(self.endCoords, forKey: "end coords")
         
     }
     
     
+    // MARK: Methods
     
+    func endTrip() {
+        
+        // get ending date
+        self.endDate = NSDate()
+        
+    }
     
-    
-    
+    public override func filePath() -> NSString {
+        
+        let tripsFolder = rootFolder.stringByAppendingPathComponent("trips") as NSString
+        return (tripsFolder.stringByAppendingPathComponent(guID.UUIDString) as NSString).stringByAppendingPathComponent("trip")
+        
+    }
     
 }
