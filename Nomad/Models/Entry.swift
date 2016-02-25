67b4ba203a347415408ac63eceebf4ef1257e792
jgal <jgal@purdue.edu>
2016-02-25 15:14:52 -0500

    Did initializers and the NSCoder stuff in Entry

--- Nomad/Models/Entry.swift
+++ Nomad/Models/Entry.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Entry: Model {
+public class Entry: Model {
     
     // MARK: Properties
     
@@ -20,40 +20,58 @@ class Entry: Model {
     var photo: Photo?
     var coords: Float?
     
-    init(title: String, date: NSDate, info: String, photo: Photo, coords: Float) {
+    init(_title: String, _info: String, _photo: Photo, _coords: Float) {
         // constructor for loading from the disk
         
-        self.title = title
-        self.date = date
-        self.info = info
-        self.photo = photo
-        self.coords = coords
+        title = _title
+        date = NSDate()
+        info = _info
+        photo = _photo
+        coords = _coords
         
          super.init(guid: NSUUID())
         
     }
     
     //When loading off of the disk
-    init?(title: String, date: NSDate, info: String, photo: Photo, coords: Float, guid: NSUUID) {
+    init?(_title: String, _date: NSDate, _info: String, _photo: Photo, _coords: Float, _guid: NSUUID) {
         // constructor used for initial creation
-        self.title = title
-        self.info = info
-        self.photo = photo
-        self.coords = coords
-        self.date = date
+        title = _title
+        info = _info
+        photo = _photo
+        coords = _coords
+        date = _date
         
-        super.init(guid: guid)
+        super.init(guid: _guid)
         
     }
     
-    required convenience public init?(coder dcoder: NSCoder) {
+    required convenience public init?(coder decoder: NSCoder) {
         guard let title = decoder.decodeObjectForKey("title") as? String,
-        let 
-    }
-
-    required convenience internal init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        let date = decoder.decodeObjectForKey("date") as? NSDate,
+        let info = decoder.decodeObjectForKey("info") as? String,
+        let photo = decoder.decodeObjectForKey("photo") as? Photo,
+        let coords = decoder.decodeObjectForKey("coords") as? Float,
+        let guid = decoder.decodeObjectForKey("guid") as? NSUUID            else {return nil}
+        
+        self.init(
+            _title: title,
+            _date: date,
+            _info: info,
+            _photo: photo,
+            _coords: coords,
+            _guid: guid
+        )
+        
     }
     
+    public override func encodeWithCoder(coder: NSCoder) {
+        super.encodeWithCoder(coder)
     
+        coder.encodeObject(self.title, forKey: "title")
+        coder.encodeObject(self.date, forKey: "date")
+        coder.encodeObject(self.info, forKey: "info")
+        coder.encodeObject(self.photo, forKey: "photo")
+        coder.encodeObject(self.coords, forKey: "coords")
+    }
 }
