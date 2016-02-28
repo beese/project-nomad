cc0d7f35b0158e82271123dc2f61c2dc449fa140
beese <kristinbeese@gmail.com>
2016-02-28 00:53:19 -0500

    home view controller buttons

--- Nomad/View Controllers/HomeViewController.swift
+++ Nomad/View Controllers/HomeViewController.swift
@@ -12,11 +12,40 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var viewTripsButton: UIButton!
+    @IBOutlet weak var addEntryButton: UIButton!
+    
+    // temp bool for code
+    // when we have models we will determine current status
+    var onTrip = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
+        // if statements to decide which buttons to show
+        
+        addEntryButton.hidden = true
+        
+        // if user is on a trip
+        // onTrip is temporary, in the future we will access model functions
+        if (onTrip) {
+            
+            addEntryButton.hidden = false
+            startButton.backgroundColor = UIColor(red: 0.9615, green: 0.7353, blue: 0.724, alpha: 1.0)
+            startButton.setTitle("End Trip", forState: UIControlState.Normal)
+            
+        }
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +64,10 @@ class HomeViewController: UIViewController {
         // change button to say end trip
         
         // add an add entry button
+        
+        // temporarily change boolean
+        // This will be gone when the models are connected
+        onTrip = !onTrip
     }
     
     @IBAction func viewTripsTapped(sender: AnyObject) {
@@ -45,4 +78,10 @@ class HomeViewController: UIViewController {
         self.navigationController?.pushViewController(TripsTableViewController(), animated: true)
     }
 
+    @IBAction func addEntryTapped(sender: AnyObject) {
+        // code for when user taps add entry
+        
+        self.navigationController?.pushViewController(AddEntryViewController(), animated: true)
+        
+    }
 }
