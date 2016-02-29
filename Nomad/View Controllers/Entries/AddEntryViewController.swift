26c1c61f135f1a096d72095554ed1dd5790f63b6
Rajalakshmy Iyer <supriya.iyer@hotmail.com>
2016-02-29 13:20:58 -0500

    Update AddEntryViewController

--- Nomad/View Controllers/Entries/AddEntryViewController.swift
+++ Nomad/View Controllers/Entries/AddEntryViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class AddEntryViewController: UIViewController {
+class AddEntryViewController: UIViewController,UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var entryLable: UILabel!
+    @IBOutlet weak var titleTextBox: UITextField!
+    @IBOutlet weak var infoLable: UILabel!
+    @IBOutlet weak var infoTextBox: UITextField!
+    @IBOutlet weak var photoLable: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Add an Entry"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Save, target: self, action: "saveTapped"
 
         // Do any additional setup after loading the view.
     }
@@ -20,8 +29,35 @@ class AddEntryViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func saveTapped() {
+        print("Entry Save")
+        let title = titleTextBox.text
+        let info = infoTextBox.text
+        let photo = photoImageBox.image
+        
+        let travel = Entry(_title: title!, _info: info!, _photo: photo!, coords: nil)
+        travel!.save()
+        
+        self.navigationController?.popViewControllerAnimated(true)
+    }
     
+    // MARK: - Actions
 
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        titleTextBox.resignFirstResponder()
+        infoTextBox.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        // The method asks ViewController to present the view controller defined by imagePickerController.
+        presentViewController(imagePickerController, animated: true, completion: nil)
+
+    }
+    
     /*
     // MARK: - Navigation
 
