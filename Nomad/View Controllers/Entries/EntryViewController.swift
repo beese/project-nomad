e18fbb339a6edcf3bedbd3ab624cffe3f15a796a
jgal <jgal@purdue.edu>
2016-04-05 01:39:53 -0400

    Removed coordinates from EntryViewController display

--- Nomad/View Controllers/Entries/EntryViewController.swift
+++ Nomad/View Controllers/Entries/EntryViewController.swift
@@ -63,7 +63,7 @@ class EntryViewController: UITableViewController {
         } else {
             entryInfo = ""
         }
-        cell.textLabel?.text = "\(entry.title)\non \(time)\nat \(entry.coords)\n\(entryInfo)\n"
+        cell.textLabel?.text = "\(entry.title)\non \(time)\n\(entryInfo)\n"
         
         cell.textLabel?.numberOfLines = 0
         return cell
