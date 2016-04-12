54866dbacaf406d2c12155a97e489395edee63ce
beese <kristinbeese@gmail.com>
2016-04-11 22:51:03 -0400

    loading screen

--- Nomad/Nomad/AppDelegate.swift
+++ Nomad/Nomad/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate   {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        usleep(500)
+        
         // when application opens
         // bring user to home view controller
         
